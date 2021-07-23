6574b2af4d2f0b2d7b723cfbfc8d53bc8896163e
Jacob Iannarino <jake@Jacobs-MacBook-Air.local>
2021-07-23 18:19:59 -0400

    removed shit i didn't want, played around with some css and html

--- config.js
+++ config.js
@@ -43,33 +43,15 @@ const CONFIG = {
     },
     {
       id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.protonmail.com/',
-    },
-    {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
-    },
-    {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
-    },
-    {
-      id: '5',
       name: 'Reddit',
       icon: 'bookmark',
       link: 'https://reddit.com',
     },
     {
-      id: '6',
-      name: 'Odysee',
+      id: '3',
+      name: 'Youtube',
       icon: 'youtube',
-      link: 'https://odysee.com/',
+      link: 'https://youtube.com',
     },
   ],
 
